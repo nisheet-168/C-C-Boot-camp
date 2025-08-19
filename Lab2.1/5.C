d1fccf743939df1f8ffb981602744f20cbaca8f6
Nisheet Trivedi <nisheettrivedi@gmail.com>
2025-08-19 19:14:51 +0530

    5.C

---
+++ Lab2.1/5.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+
+    printf("Enter value for a: ");
+    scanf("%d", &a);
+    printf("Enter value for b: ");
+    scanf("%d", &b);
+    
+    printf("\nBefore swapping:\n");
+    printf("a = %d, b = %d\n", a, b);
+
+
+    
+    printf("\nAfter swapping:\n");
+    printf("a = %d, b = %d\n", b,a);
+
+    return 0;
+}
