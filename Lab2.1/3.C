85eff856bbd3837cb1781a64fb79c168753cfc23
Nisheet Trivedi <nisheettrivedi@gmail.com>
2025-08-19 19:13:02 +0530

    3.C

---
+++ Lab2.1/3.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    double x, y, result;
+    printf("Enter value for x: ");
+    scanf("%lf", &x);
+    printf("Enter value for y: ");
+    scanf("%lf", &y);
+
+    result = (x + y) * (x + y) * (x + y);
+
+    printf("(%.2f + %.2f)^3 = %.2f\n", x, y, result);
+
+    return 0;
+}
