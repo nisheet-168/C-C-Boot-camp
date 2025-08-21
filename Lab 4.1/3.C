63316525904e73db7ba9ac95949e586a825e920f
Nisheet Trivedi <nisheettrivedi@gmail.com>
2025-08-21 16:37:34 +0530

    3.C
    Write program to print 1 to N using while loop

--- Lab 4.1/3.C
+++ Lab 4.1/3.C
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int N;
+    int N, i = 1;
+
     printf("Enter any number: ");
     scanf("%d", &N);
 
-    while (N >= 1) {
-        if (N % 2 != 0) {
-            printf("%d\n ", N);
-        }
-        N--;
+    while (i <= N) {
+        printf("%d\n", i);
+        i++;
     }
 
     return 0;
