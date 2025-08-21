44ff46a90847bda72acd791802a9d2b80713f41f
Nisheet Trivedi <nisheettrivedi@gmail.com>
2025-08-21 22:18:26 +0530

    3.C

--- lab1.2/3.C
+++ lab1.2/3.C
@@ -4,18 +4,16 @@ int main()
  {
     float b,h, area;
 
-    // Prompt user for radius
     printf("Enter the base of the triangme: ");
     scanf("%f", &b);
     
     printf("Enter the height of the triangle: ");
     scanf("%f", &h);
 
-    // Calculate area
     area=0.5*(b*h);
 
-    // Display result
     printf("The area of the traingel is: %.2f\n", area);
 
     return 0;
 }
+
