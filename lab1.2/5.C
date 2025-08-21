a7e249d4a43258c64fff44ae67fd7640e11bc7ee
Nisheet Trivedi <nisheettrivedi@gmail.com>
2025-08-21 22:20:20 +0530

    5.C

--- lab1.2/5.C
+++ lab1.2/5.C
@@ -5,15 +5,13 @@ int main()
  {
     float radius,perimeter;
 
-    // Prompt user for radius
     printf("Enter the radius of the circle: ");
     scanf("%f", &radius);
-
-    // Calculate area
+  
     perimeter = 2* PI * radius;
 
-    // Display result
     printf("The perimiter of the circle is: %.2f\n", perimeter);
 
     return 0;
 }
+
